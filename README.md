a10f5226c14c77e24cc01ca40e5f84e9d07ce661
snillocmot <tom.collins.work@gmail.com>
2014-03-03 14:10:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# SMARTLIVING.IO
+
+Landing page..
---
+++ src/raw/manycontacts.js
@@ -0,0 +1,7 @@
+(function() {
+	window._EP = "https://www.manycontacts.com";
+	window.__bID = "53143c5e80df1a13714d3d9e";
+	document.body.appendChild(document.createElement("script")).src = window._EP
+			+ "/assets/js/manycontacts.min.js";
+
+}).call(this);
